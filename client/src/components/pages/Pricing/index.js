9e85e05874665d3f54217e962049ac6610d2df56
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-11-07 18:32:22 -0500

    added rough layout to the Pricing component.

--- client/src/components/pages/Pricing/index.js
+++ client/src/components/pages/Pricing/index.js
@@ -1,9 +1,69 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import { AppBar, Tabs, Tab, Typography, Box } from "@material-ui/core";
 
-const Pricing = props => {
+const TabPanel = props => {
+	const { children, value, index, ...other } = props;
+
 	return (
-		<div>
-            Pricing Placeholder
+		<Typography
+			component="div"
+			role="tabpanel"
+			hidden={value !== index}
+			id={`simple-tabpanel-${index}`}
+			aria-labelledby={`simple-tab-${index}`}
+			{...other}
+		>
+			<Box p={3}>{children}</Box>
+		</Typography>
+	);
+};
+
+TabPanel.propTypes = {
+	children: PropTypes.node,
+	index: PropTypes.any.isRequired,
+	value: PropTypes.any.isRequired,
+};
+
+const allyProps = index => {
+	return {
+		id: `simple-tab-${index}`,
+		"aria-controls": `simple-tabpanel-${index}`,
+	};
+};
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		flexGrow: 1,
+		backgroundColor: theme.palette.background.paper,
+	},
+}));
+
+const Pricing = () => {
+	const classes = useStyles();
+	const [value, setValue] = React.useState(0);
+
+	const handleChange = (event, newValue) => setValue(newValue);
+
+	return (
+		<div className={classes.root}>
+			<AppBar position="static">
+				<Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+					<Tab label="Item One" {...allyProps(0)} />
+					<Tab label="Item Two" {...allyProps(1)} />
+					<Tab label="Item Three" {...allyProps(2)} />
+				</Tabs>
+			</AppBar>
+			<TabPanel value={value} index={0}>
+				Item One
+			</TabPanel>
+			<TabPanel value={value} index={1}>
+				Item Two
+			</TabPanel>
+			<TabPanel value={value} index={2}>
+				Item Three
+			</TabPanel>
 		</div>
 	);
 };
