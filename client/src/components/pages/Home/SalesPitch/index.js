de9fbbb511347c7ae9838d33d0ba4d3c8ae68dcb
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-11-14 16:55:19 -0500

    updated text data for home page.

--- client/src/components/pages/Home/SalesPitch/index.js
+++ client/src/components/pages/Home/SalesPitch/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Paper, Typography, Grid } from "@material-ui/core";
+import { Paper, Typography, Grid, Divider } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
 	root: {
@@ -9,6 +9,9 @@ const useStyles = makeStyles(theme => ({
 		height: "100%",
 		width: "100%"
 	},
+	subtitle: {
+		textAlign: "center"
+	}
 }));
 
 const SalesPitch = () => {
@@ -17,20 +20,32 @@ const SalesPitch = () => {
 	return (
 		<Grid item xs={12} md={8}>
 			<Paper className={classes.root}>
-				<Typography variant="h5" component="h3">
-				Who benefits from our services?
+				<Typography variant="h5" component="h3" className={classes.subtitle}>
+					Who benefits from our services?
+				</Typography>
+				<Divider />
+				<Typography variant="subtitle1" gutterBottom>
+					Retailers.
 				</Typography>
 
-				<Typography component="p">
-                    Retailers. We offer a simple way to get detailed information to customers about your products and services or trying to train new employees on your store's intricacies while giving them a tour of the facilities.
+				<Typography variant="body1" gutterBottom>
+					We offer a simple way to get detailed information to customers about your products and services or trying to train new employees on your store's intricacies while giving them a tour of the facilities.
 				</Typography>
 
-				<Typography component="p">
-                    School Campuses. We provide an easy way to offer students and parents a closer look at your school's facilities and goals when it comes to educating future generations.
+				<Typography variant="subtitle1" gutterBottom>
+					School Campuses.
 				</Typography>
 
-				<Typography component="p">
-                    National Parks. Education outreach is an important way our National Parks system keeps new generations of Americans informed about the natural world around them. Placing our codes throughout the park offers additional insight into mother nature with just a quick scan. 
+				<Typography variant="body1" gutterBottom>
+					We provide an easy way to offer students and parents a closer look at your school's facilities and goals when it comes to educating future generations.
+				</Typography>
+
+				<Typography variant="subtitle1" gutterBottom>
+					National Parks.
+				</Typography>
+
+				<Typography variant="body1" gutterBottom>
+					Education outreach is an important way our National Parks system keeps new generations of Americans informed about the natural world around them. Placing our codes throughout the park offers additional insight into mother nature with just a quick scan.
 				</Typography>
 
 			</Paper>
