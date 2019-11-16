92b15ebbec8e0dacf1c46442de9c4e27fea7e06b
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-11-16 14:17:58 -0500

    linter run on routes folder

--- routes/api/ToqrDB.js
+++ routes/api/ToqrDB.js
@@ -2,14 +2,14 @@ const router = require("express").Router();
 const toqrController = require("../../controllers/toqrController");
 
 router
-    .route("/")
-    .get(toqrController.findAll)
-    .post(toqrController.create);
+	.route("/")
+	.get(toqrController.findAll)
+	.post(toqrController.create);
 
 router
-    .route("/:id")
-    .get(toqrController.findById)
-    .put(toqrController.update)
-    .delete(toqrController.remove);
+	.route("/:id")
+	.get(toqrController.findById)
+	.put(toqrController.update)
+	.delete(toqrController.remove);
 
 module.exports = router;
