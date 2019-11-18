586404d756a75f847c5b127e8224ef055ea29848
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-11-17 23:03:24 -0500

    separated basic navbar from navdrawer. navbar disappears between mobile and desktop sizes. unsure why, more testing on responsiveness next session.

--- client/src/components/NavDrawer/index.js
+++ client/src/components/NavDrawer/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import TopBarNav from "./TopBarNav";
 import {
 	AppBar,
 	CssBaseline,
@@ -17,9 +18,6 @@ import {
 import InfoIcon from "@material-ui/icons/Info";
 import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
 import EmailIcon from "@material-ui/icons/Email";
-import AccountBoxIcon from "@material-ui/icons/AccountBox";
-import SettingsIcon from "@material-ui/icons/Settings";
-import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import MenuIcon from "@material-ui/icons/Menu";
 import HomeIcon from "@material-ui/icons/Home";
 import ImportExportIcon from "@material-ui/icons/ImportExport";
@@ -51,6 +49,9 @@ const useStyles = makeStyles(theme => ({
 			display: "none"
 		}
 	},
+	list: {
+		width: 250,
+	},
 	toolbar: theme.mixins.toolbar,
 	drawerPaper: {
 		width: drawerWidth
@@ -74,14 +75,22 @@ const useStyles = makeStyles(theme => ({
 const NavDrawer = props => {
 	const { container } = props;
 	const classes = useStyles();
-	const theme = useTheme();
-	const [mobileOpen, setMobileOpen] = useState(false);
+	const [state, setState] = useState({
+		top: false,
+		left: false,
+		bottom: false,
+		right: false,
+	});
 
-	const handleDrawerToggle = () => {
-		setMobileOpen(!mobileOpen);
+	const toggleDrawer = (side, open) => event => {
+		if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+			return;
+		}
+
+		setState({ ...state, [side]: open });
 	};
 
-	const navListArr = ["Home", "About", "Pricing", "Contact", "QRForm"];
+	const navListArr = ["Home", "Pricing", "QRForm", "Contact", "About"];
 
 	const routeLink = route => {
 		switch (route) {
@@ -113,9 +122,13 @@ const NavDrawer = props => {
 		}
 	}
 
-	const drawer = (
-		<div>
-			<div className={classes.toolbar} />
+	const sideList = side => (
+		<div
+			className={classes.list}
+			role="presentation"
+			onClick={toggleDrawer(side, false)}
+			onKeyDown={toggleDrawer(side, false)}
+		>
 			<Divider />
 			<List>
 				{navListArr.map(text => (
@@ -129,69 +142,164 @@ const NavDrawer = props => {
 					</Link>
 				))}
 			</List>
-		</div >
+		</div>
 	);
 
 	return (
 		<Router>
 			<div className={classes.root}>
 				<CssBaseline />
-				<AppBar position="fixed" className={classes.appBar}>
-					<Toolbar>
-						<IconButton
-							color="inherit"
-							aria-label="open drawer"
-							edge="start"
-							onClick={handleDrawerToggle}
-							className={classes.menuButton}
-						>
-							<MenuIcon />
-						</IconButton>
+				<Hidden smUp>
+					<AppBar position="fixed" className={classes.appBar}>
+						<Toolbar>
+							<IconButton
+								color="inherit"
+								aria-label="open drawer"
+								edge="start"
+								onClick={toggleDrawer('left', true)}
+								className={classes.menuButton}
+							>
+								<MenuIcon />
+							</IconButton>
+						</Toolbar>
+					</AppBar>
 
-						<Grid container>
-							<Hidden xsDown>
-								<Grid item sm={5} className={classes.logoPad}>
-									<Typography variant="h6" noWrap>
-										<Link to="/" className={classes.unlink}>TOQR</Link>
-									</Typography>
-								</Grid>
-							</Hidden>
-						</Grid>
-					</Toolbar>
-				</AppBar>
-				<nav className={classes.drawer} aria-label="mailbox folders">
-					<Hidden smUp implementation="css">
-						<Drawer
-							container={container}
-							variant="temporary"
-							anchor={theme.direction === "rtl" ? "right" : "left"}
-							open={mobileOpen}
-							onClose={handleDrawerToggle}
-							classes={{
-								paper: classes.drawerPaper
-							}}
-							ModalProps={{
-								keepMounted: true
-							}}
-						>
-							{drawer}
-						</Drawer>
-					</Hidden>
-					<Hidden xsDown implementation="css">
-						<Drawer
-							classes={{
-								paper: classes.drawerPaper
-							}}
-							variant="permanent"
-							open
-						>
-							{drawer}
-						</Drawer>
-					</Hidden>
-				</nav>
+					<Drawer open={state.left} onClose={toggleDrawer('left', false)}>
+						{sideList('left')}
+					</Drawer>
+				</Hidden>
+
+				<Hidden mdDown>
+					<TopBarNav />
+				</Hidden>
 				{props.children}
 			</div>
 		</Router>
 	);
 };
+
+
+// const NavDrawer = props => {
+// 	const { container } = props;
+// 	const classes = useStyles();
+// 	const theme = useTheme();
+// 	const [mobileOpen, setMobileOpen] = useState(false);
+
+// 	const handleDrawerToggle = () => {
+// 		setMobileOpen(!mobileOpen);
+// 	};
+
+// 	const navListArr = ["Home", "About", "Pricing", "Contact", "QRForm"];
+
+// 	const routeLink = route => {
+// 		switch (route) {
+// 			case "Home":
+// 				return `/`
+// 			case "About":
+// 				return `/${route}`
+// 			case "Pricing":
+// 				return `/${route}`
+// 			case "QRForm":
+// 				return `/${route}`
+// 			case "Contact":
+// 				return `/${route}`
+// 		}
+// 	}
+
+// 	const navListSwitch = text => {
+// 		switch (text) {
+// 			case "Home":
+// 				return <HomeIcon />
+// 			case "About":
+// 				return <InfoIcon />
+// 			case "Pricing":
+// 				return <MonetizationOnIcon />
+// 			case "QRForm":
+// 				return <ImportExportIcon />
+// 			case "Contact":
+// 				return <EmailIcon />
+// 		}
+// 	}
+
+// 	const drawer = (
+// 		<div>
+// 			<div className={classes.toolbar} />
+// 			<Divider />
+// 			<List>
+// 				{navListArr.map(text => (
+// 					<Link to={routeLink(text)} className={classes.unlink} key={text}>
+// 						<ListItem button key={text}>
+// 							<ListItemIcon>
+// 								{navListSwitch(text)}
+// 							</ListItemIcon>
+// 							<ListItemText primary={text} />
+// 						</ListItem>
+// 					</Link>
+// 				))}
+// 			</List>
+// 		</div >
+// 	);
+
+// 	return (
+// 		<Router>
+// 			<div className={classes.root}>
+// 				<CssBaseline />
+// 				<AppBar position="fixed" className={classes.appBar}>
+// 					<Toolbar>
+// 						<IconButton
+// 							color="inherit"
+// 							aria-label="open drawer"
+// 							edge="start"
+// 							onClick={handleDrawerToggle}
+// 							className={classes.menuButton}
+// 						>
+// 							<MenuIcon />
+// 						</IconButton>
+
+// 						<Grid container>
+// 							<Hidden xsDown>
+// 								<Grid item sm={5} className={classes.logoPad}>
+// 									<Typography variant="h6" noWrap>
+// 										<Link to="/" className={classes.unlink}>TOQR</Link>
+// 									</Typography>
+// 								</Grid>
+// 							</Hidden>
+// 						</Grid>
+// 					</Toolbar>
+// 				</AppBar>
+// 				<nav className={classes.drawer} aria-label="mailbox folders">
+// 					<Hidden smUp implementation="css">
+// 						<Drawer
+// 							container={container}
+// 							variant="temporary"
+// 							anchor={theme.direction === "rtl" ? "right" : "left"}
+// 							open={mobileOpen}
+// 							onClose={handleDrawerToggle}
+// 							classes={{
+// 								paper: classes.drawerPaper
+// 							}}
+// 							ModalProps={{
+// 								keepMounted: true
+// 							}}
+// 						>
+// 							{drawer}
+// 						</Drawer>
+// 					</Hidden>
+// 					<Hidden xsDown implementation="css">
+// 						<Drawer
+// 							classes={{
+// 								paper: classes.drawerPaper
+// 							}}
+// 							variant="permanent"
+// 							open
+// 						>
+// 							{drawer}
+// 						</Drawer>
+// 					</Hidden>
+// 				</nav>
+// 				{props.children}
+// 			</div>
+// 		</Router>
+// 	);
+// };
 export default NavDrawer;
